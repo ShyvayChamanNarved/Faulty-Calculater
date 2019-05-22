c9ec8e7a84fe90a0a2a4c52624047b9a41ed4900
Chaman narved <chamannarved05@gmail.com>
2019-05-22 22:30:23 +0530

    Basic Calculater features

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Faulty-Calculater
+
+Basic calculator program using Python. Which can perform basic arithmetic operations like addition, subtraction, multiplication or division. But some faulty values calculated wrong.
+These faulty values are: 45*3=555, 56+9=77,56/6=4
---
+++ faulty-calculater.py
@@ -0,0 +1,25 @@
+#Faulty Calculator
+#45*3=555, 56+9=77,56/6=4
+
+num1=int(input("Enter first value: "))
+num2=int(input("Enter second value: "))
+op=input("Enter operetor: ")
+if num1==45 and num2==3 and op=="*":
+    print("Result: 555")
+elif num1==56 and num2==9 and op=="+":
+    print("Result: 77")
+elif num1==56 and num2==6 and op=="/":
+    print("Result: 4")
+else:
+    if op == "+":
+        print("Result: ", num1 + num2)
+    elif (op == "-"):
+        print("Result: ", num1 - num2)
+    elif (op == "*"):
+        print("Result: ", num1 * num2)
+    elif (op == "/"):
+        print("Result: ", num1 / num2)
+    elif (op == "%"):
+        print("Result: ", num1 % num2)
+    else:
+        print("invalid operetor")
