2d1bbb97862dc5197d11ea5a503671c3d6a29bc4
Chaman narved <chamannarved05@gmail.com>
2019-08-29 15:27:38 +0530

    new commit

--- faulty-calculater.py
+++ faulty-calculater.py
@@ -4,6 +4,7 @@
 num1=int(input("Enter first value: "))
 num2=int(input("Enter second value: "))
 op=input("Enter operetor: ")
+
 if num1==45 and num2==3 and op=="*":
     print("Result: 555")
 elif num1==56 and num2==9 and op=="+":
